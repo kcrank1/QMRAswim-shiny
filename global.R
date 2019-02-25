393f0e91b4326ce99aa45656da18edaff2307272
Lance Tyler Upton <lance.upton@gmail.com>
2019-02-25 13:35:41 -0500

    Implement better package handling to accommodate first-time users.

--- global.R
+++ global.R
@@ -1,8 +1,13 @@
 
-###Copy of app.R, with no for loops
-library(shiny)
-library(CharFun)
-library(reshape2)
-library(ggplot2)
-library(scales)
-library(shinydashboard)
+rm(list = ls())
+
+# Handle packages
+lapply(
+  X = c('shiny', 'CharFun', 'reshape2', 'ggplot2', 'scales', 'shinydashboard'),
+  FUN = function(p) {
+    if(p %in% installed.packages()[,'Package'] == FALSE) {
+      install.packages(p, dependencies = TRUE)
+    }
+    library(p, character.only = TRUE)
+  }
+)
