c7ea08023b31d3ffcc504d89500c506a4203c9ad
Lance Tyler Upton <lance.upton@gmail.com>
2019-02-25 15:07:53 -0500

    Migrate text blocks to global script for tidyness.

--- global.R
+++ global.R
@@ -11,3 +11,21 @@ lapply(
     library(p, character.only = TRUE)
   }
 )
+
+
+text_blocks <- list(
+  appinfo = paste(
+    "QMRAswim is a web tool that models the range of probability of illnesses from swimming in a sewage impacted water body.",
+    "The user inputs the measured concentration of the indicator organism and inputs desired reference values, and the output is a histogram of probability of illness from pathogens the user inputs data for.",
+    "Downloads will be available for every computational step in the form of an Excel document.",
+    "The intended audience for this tool are scientists or public health professionals who have access to the wide variety of reference information that is required to run the tool.",
+    "Currently the web tool is coded in R Shiny, but will ideally be published on the web for general use.",
+    "What makes this tool useful and unique it can take one measurement from an impacted water body and tell the user ranges of probabilities of illness for any number or type of pathogen."
+  ),
+  doseresponseinfo = paste(
+    "Below is the default values for water consumed by a swimmer in mL.",
+    "Water dose is from 'Water ingestion during swimming activities in a pool: a pilot study', lognormal distribution with a mean of 2.92 and a standard deviation of 1.43 (Dufor et al. 2006).",
+    "The parameters can be changed but using any distribution besides log normal is not currently supported."
+  )
+)
+
--- server.R
+++ server.R
@@ -1,17 +1,9 @@
 
 # Define server logic
 shinyServer(function(input, output, session) {
-  output$appinfo<-renderText(print("QMRAswim is a web tool that models the range of probability of illnesses from swimming in a sewage impacted water body. The user inputs the measured concentration of the indicator organism and inputs desired reference values, and the output is a histogram of probability of illness from pathogens the user inputs data for. Downloads will be available for every computational step in the form of an Excel document.
 
-                                   The intended audience for this tool are scientists or public health professionals who have access to the wide variety of reference information that is required to run the tool.
-
-                                   Currently the web tool is coded in R Shiny, but will ideally be published on the web for general use.
-
-                                   What makes this tool useful and unique it can take one measurement from an impacted water body and tell the user ranges of probabilities of illness for any number or type of pathogen.
-                                   "))
-  output$doseresponseinfo <-renderText(
-    print("Below is the default values for water consumed by a swimmer in mL. Water dose is from 'Water ingestion during swimming activities in a pool: a pilot study', lognormal distribution with a mean of 2.92 and a standard deviation of 1.43 (Dufor et al. 2006). The parameters can be changed but using any distribution besides log normal is not currently supported.")
-  )
+  output$appinfo <- renderText(print(text_blocks$appinfo))
+  output$doseresponseinfo <- renderText(print(text_blocks$doseresponseinfo))
 
   # Wastewater Dose function
   #WastewaterDose<-function(input.indic_enviro_conc, input.indic_sewage_dist='Log Uniform',input.min,input.max, input.alpha, input.beta, input.seed =1, input.count=10000,input.dosemean, input.dosesd){
