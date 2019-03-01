9540699487038ad46ca5ec05a4fb1deb39422e45
kcrank1 <kcrank@nd.edu>
2019-03-01 17:09:53 -0500

    Increased the supported amount of pathogens from 3 to 7

--- server.R
+++ server.R
@@ -1,11 +1,26 @@
 
 shinyServer(function(input, output, session) {
+  
+   # For loop for module assignment 
+  #Load modules
+  path_num_vector<-reactive(1:input$path_num)
 
-  # Load modules
+  # for (i in path_num_vector()) {
+  #   assign(paste0("path",i),callModule(module= pathogens, id= paste0("path,i"),n=reactive(i),sample_n=sample_n, wwdose_wwdose))
+  # 
+  # }
+  # paths<-reactive({
+  #   lapply(path_num_vector(), function(x)  assign(paste0("path",x),
+  #                                                  callModule(module = pathogens, id = paste0("path,i"), n = reactive(x), sample_n = sample_n, ww_dose = ww_dose)
+  #          ))
+  # })
   path1 <- callModule(module = pathogens, id = 'path1', n = reactive(1), sample_n = sample_n, ww_dose = ww_dose)
   path2 <- callModule(module = pathogens, id = 'path2', n = reactive(2), sample_n = sample_n, ww_dose = ww_dose)
   path3 <- callModule(module = pathogens, id = 'path3', n = reactive(3), sample_n = sample_n, ww_dose = ww_dose)
-
+  path4 <- callModule(module = pathogens, id = 'path4', n = reactive(4), sample_n = sample_n, ww_dose = ww_dose)
+  path5 <- callModule(module = pathogens, id = 'path5', n = reactive(5), sample_n = sample_n, ww_dose = ww_dose)
+  path6 <- callModule(module = pathogens, id = 'path6', n = reactive(6), sample_n = sample_n, ww_dose = ww_dose)
+  path7 <- callModule(module = pathogens, id = 'path7', n = reactive(7), sample_n = sample_n, ww_dose = ww_dose)
   # UI ----------------------------------------------------------------------
 
   output$ui <- renderUI({
@@ -192,9 +207,9 @@ shinyServer(function(input, output, session) {
   output$ui_path_num <- renderUI({
     numericInput(
       inputId = "path_num",
-      label   = "How many pathogen dose responses to include, up to 3 currently supported.",
+      label   = "How many pathogen dose responses to include, up to 7 currently supported.",
       min     = 1,
-      max     = 3,
+      max     = 7,
       value   = 1,
       step    = 1
     )
@@ -369,10 +384,24 @@ shinyServer(function(input, output, session) {
     } else if(input$path_num == 2) {
       1 - ( (1 - path1$p_ill()) * (1 - path2$p_ill()) )
     } else if(input$path_num == 3) {
-      1 - ( (1 - path1$p_ill()) * (1 - path2$p_ill()) * (1 - path3$p_ill()) )
-    }
+      1 - ( (1 - path1$p_ill()) * (1 - path2$p_ill()) * (1 - path3$p_ill()))
+    } else if(input$path_num == 4) {
+      1 - ( (1 - path1$p_ill()) * (1 - path2$p_ill()) * (1 - path3$p_ill()) * (1 - path4$p_ill()) )
+    } else if(input$path_num == 5) {
+      1 - ( (1 - path1$p_ill()) * (1 - path2$p_ill()) * (1 - path3$p_ill()) * (1 - path4$p_ill()) * (1 - path5$p_ill()))
+    } else if(input$path_num == 6) {
+      1 - ( (1 - path1$p_ill()) * (1 - path2$p_ill()) * (1 - path3$p_ill()) * (1 - path4$p_ill()) * (1 - path5$p_ill())* (1 - path6$p_ill()))
+    } else if(input$path_num == 7) {
+      1 - ( (1 - path1$p_ill()) * (1 - path2$p_ill()) * (1 - path3$p_ill()) * (1 - path4$p_ill()) * (1 - path5$p_ill())* (1 - path6$p_ill())* (1 - path7$p_ill()))
+  }
   })
-
+#  p_ill_tot <-reactive({
+#   req(input$path_num)
+#   1-(1-path$p_ill())^input$path_num# RENAME INPUT$PATH_NUM  towards beginning of server path_num<-reactive(input$path_num)
+# })
+  
+  #loop for i in 1:n add column to data frame of path add names as you add columns, create list of names or vector of names
+  #create list of length input$path_numm that contains 
   df <- reactive({
     req(input$path_num)
     if(input$path_num == 1) {
@@ -384,9 +413,23 @@ shinyServer(function(input, output, session) {
     } else if(input$path_num == 3) {
       data.frame(path1$p_ill(), path2$p_ill(), path3$p_ill(), p_ill_tot()) %>%
         set_names(nm = c(path1$name(), path2$name(), path3$name(), "Total Pill"))
+    } else if(input$path_num == 4) {
+      data.frame(path1$p_ill(), path2$p_ill(), path3$p_ill(), path4$p_ill(), p_ill_tot()) %>%
+        set_names(nm = c(path1$name(), path2$name(), path3$name(), path4$name(), "Total Pill"))
+    } else if(input$path_num == 5) {
+      data.frame(path1$p_ill(), path2$p_ill(), path3$p_ill(), path4$p_ill(), path5$p_ill(), p_ill_tot()) %>%
+        set_names(nm = c(path1$name(), path2$name(), path3$name(), path4$name(), path5$name(), "Total Pill"))
+    } else if(input$path_num == 6) {
+      data.frame(path1$p_ill(), path2$p_ill(), path3$p_ill(), path4$p_ill(), path5$p_ill(), path6$p_ill(),  p_ill_tot()) %>%
+        set_names(nm = c(path1$name(), path2$name(), path3$name(), path4$name(), path5$name(), path6$name(), "Total Pill"))
+    } else if(input$path_num == 7) {
+      data.frame(path1$p_ill(), path2$p_ill(), path3$p_ill(), path4$p_ill(), path5$p_ill(), path6$p_ill(), path7$p_ill(),  p_ill_tot()) %>%
+        set_names(nm = c(path1$name(), path2$name(), path3$name(), path4$name(), path5$name(), path6$name(), path7$name(), "Total Pill"))
     }
   })
 
+  
+  
   df_final <- reactive({
     melt(
       data          = df(),
